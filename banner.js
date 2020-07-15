f26ffa00cf4df667ec0129e7fe65b5a780aa9d2c
Di-homo <68106113+Di-homo@users.noreply.github.com>
2020-07-15 14:06:50 +0300

    change codestyle
    var instead let, comments

--- banner.js
+++ banner.js
@@ -1,11 +1,10 @@
 //add company banner
 
 addCompanyBanner('login_body');
-http://dinacheley.com/wialon/lessons/customMenu.js
 function addCompanyBanner(parentNode)
 {
-    let doc = document;
-    let div = doc.createElement('div'); // Create <div> element
+    var doc = document;
+    var div = doc.createElement('div'); // Create <div> element
     div.id = "new_banner"; 
     //Fill it with the content
     div.innerHTML ="<a href='https://gurtam.com/en/wialon' target='_blank'><img src='http://dinacheley.com/wialon/lessons/images/1.jpg' class='img-thumbnail' style = 'display: block; margin-left: auto; margin-right: auto' alt='Logo'></a>"
